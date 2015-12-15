1349638e92f869d05f4ca65954764b8df49b648f
Gopal Chandra Bala <gopal@bitmascot.com>
2015-12-15 12:49:34 +0600

    unity implementation

---
+++ Unity_DI_Console_App/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.Practices.Unity;
+using Unity_DI_Console_App.Repository;
+
+namespace Unity_DI_Console_App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var container = new UnityContainer();
+            container.RegisterType<IUserRepository<User>, UserRepository>();
+            var usrRepo = container.Resolve<IUserRepository<User>>();
+            var lst = usrRepo.GetUsers();
+            foreach (var o in lst)
+            {
+                Console.Write(o.UserId + "   " + o.Name + "   " + o.Email);
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Unity_DI_Console_App/Repository/UserRepository.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+
+namespace Unity_DI_Console_App.Repository
+{
+    public class UserRepository : IUserRepository<User>
+    {
+        public List<User> GetUsers()
+        {
+            var ul = new List<User>
+            {
+                new User {UserId = 1, Name = "Gopal", Email = "gopal@bitmascot.com"},
+                new User {UserId = 2, Name = "Nayan", Email = "nayan@live.com"}
+            };
+            return ul;
+        } 
+    }
+    public interface IUserRepository<User>
+    {
+        List<User> GetUsers();
+    }
+}
